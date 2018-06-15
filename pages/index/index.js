d445bdf3c2dc537e2f07dddb9b824f281d41abdf
sohoorc <me@gwtcoder.me>
2018-06-15 22:33:23 +0800

    清除console

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,60 +2,59 @@
 
 Page({
   data: {
-    isActive:'1',
-    topicList:[
+    isActive: '1',
+    topicList: [
       {
-        legend:'',
-        name:'郑家3公主',
-        title:'年入一百万元是一种什么样的体验？',
-        article:'我每个月只有2000元底薪',
-        agree:55,
-        discuss:66,
-        uid:'fdsafafdiyt123'
-      },{
-        legend:'',
-        name:'甘大少',
-        title:'年入一百万元是一种什么样的体验？',
-        article:'谢邀！',
-        agree:17,
-        discuss:100,
-        uid:'fdsvvvvafafd123'
-      },{
-        legend:'',
-        name:'郊县吴孟达',
-        title:'年入一百万元是一种什么样的体验？',
-        article:' 谢邀，我月薪2500，无法回答这个问题。谢邀，我月薪2500，无法回答这个问题。',
-        agree:44,
-        discuss:33,
-        uid:'fdsafacccfd123'
-      },{
-        legend:'',
-        name:'肥皂君',
-        title:'年入一百万元是一种什么样的体验？',
-        article:'没我什么事情，没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情',
-        agree:56,
-        discuss:5,
-        uid:'fdsafaaaafd123'
-      },{
-        legend:'',
-        name:'香波君',
-        title:'年入一百万元是一种什么样的体验？',
-        article:' 谢邀，我月薪2500，无法回答这个问题。谢邀，我月薪2500，无法回答这个问题。',
-        agree:12,
-        discuss:1,
-        uid:'fdsafafdeqw123'
+        legend: '',
+        name: '郑家3公主',
+        title: '年入一百万元是一种什么样的体验？',
+        article: '我每个月只有2000元底薪',
+        agree: 55,
+        discuss: 66,
+        uid: 'fdsafafdiyt123'
+      }, {
+        legend: '',
+        name: '甘大少',
+        title: '年入一百万元是一种什么样的体验？',
+        article: '谢邀！',
+        agree: 17,
+        discuss: 100,
+        uid: 'fdsvvvvafafd123'
+      }, {
+        legend: '',
+        name: '郊县吴孟达',
+        title: '年入一百万元是一种什么样的体验？',
+        article: ' 谢邀，我月薪2500，无法回答这个问题。谢邀，我月薪2500，无法回答这个问题。',
+        agree: 44,
+        discuss: 33,
+        uid: 'fdsafacccfd123'
+      }, {
+        legend: '',
+        name: '肥皂君',
+        title: '年入一百万元是一种什么样的体验？',
+        article: '没我什么事情，没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情没我什么事情',
+        agree: 56,
+        discuss: 5,
+        uid: 'fdsafaaaafd123'
+      }, {
+        legend: '',
+        name: '香波君',
+        title: '年入一百万元是一种什么样的体验？',
+        article: ' 谢邀，我月薪2500，无法回答这个问题。谢邀，我月薪2500，无法回答这个问题。',
+        agree: 12,
+        discuss: 1,
+        uid: 'fdsafafdeqw123'
       }
     ]
   },
   // 标签栏切换
-  tabActiveToggle: function(e){
-    console.log(e)
+  tabActiveToggle: function (e) {
     // current index
     var index = e.target.dataset.index;
 
     this.setData({
       isActive: index
-  })
+    })
 
   }
 })
